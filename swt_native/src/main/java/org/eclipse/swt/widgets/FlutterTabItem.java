74038c569a92550d71eef0a4293579e28fd239f7
Elias Menon <eliasmenon@gmail.com>
2025-04-25 20:46:08 -0300

    Add FlutterTabFolder and FlutterTabItem

--- swt_native/src/main/java/org/eclipse/swt/widgets/FlutterTabItem.java
+++ swt_native/src/main/java/org/eclipse/swt/widgets/FlutterTabItem.java
@@ -1,155 +1,263 @@
 package org.eclipse.swt.widgets;
 
-import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.values.TabItemValue;
 
-import java.util.EventListener;
-import java.util.stream.Stream;
+import java.util.ArrayList;
 
-public class FlutterTabItem implements ITabItem {
-    @Override
+/**
+ * Instances of this class represent a selectable user interface object
+ * corresponding to a tab for a page in a tab folder.
+ * <dl>
+ * <dt><b>Styles:</b></dt>
+ * <dd>(none)</dd>
+ * <dt><b>Events:</b></dt>
+ * <dd>(none)</dd>
+ * </dl>
+ * <p>
+ * IMPORTANT: This class is <em>not</em> intended to be subclassed.
+ * </p>
+ *
+ * @see <a href="http://www.eclipse.org/swt/snippets/#tabfolder">TabFolder, TabItem snippets</a>
+ * @see <a href="http://www.eclipse.org/swt/">Sample code and further information</a>
+ * @noextend This class is not intended to be subclassed by clients.
+ */
+public class FlutterTabItem extends FlutterItem implements ITabItem {
+
+    FlutterTabFolder parent;
+
+    /**
+     * Constructs a new instance of this class given its parent
+     * (which must be a <code>TabFolder</code>) and a style value
+     * describing its behavior and appearance. The item is added
+     * to the end of the items maintained by its parent.
+     * <p>
+     * The style value is either one of the style constants defined in
+     * class <code>SWT</code> which is applicable to instances of this
+     * class, or must be built by <em>bitwise OR</em>'ing together
+     * (that is, using the <code>int</code> "|" operator) two or more
+     * of those <code>SWT</code> style constants. The class description
+     * lists the style constants that are applicable to the class.
+     * Style bits are also inherited from superclasses.
+     * </p>
+     *
+     * @param parent a composite control which will be the parent of the new instance (cannot be null)
+     * @param style the style of control to construct
+     *
+     * @exception IllegalArgumentException <ul>
+     *    <li>ERROR_NULL_ARGUMENT - if the parent is null</li>
+     * </ul>
+     * @exception SWTException <ul>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the parent</li>
+     *    <li>ERROR_INVALID_SUBCLASS - if this class is not an allowed subclass</li>
+     * </ul>
+     *
+     * @see SWT
+     * @see Widget#checkSubclass
+     * @see Widget#getStyle
+     */
+    public FlutterTabItem(ITabFolder parent, int style) {
+        super(parent, style);
+        this.parent = (FlutterTabFolder)parent;
+        createWidget(parent.getItemCount());
+    }
+
+    /**
+     * Constructs a new instance of this class given its parent
+     * (which must be a <code>TabFolder</code>), a style value
+     * describing its behavior and appearance, and the index
+     * at which to place it in the items maintained by its parent.
+     * <p>
+     * The style value is either one of the style constants defined in
+     * class <code>SWT</code> which is applicable to instances of this
+     * class, or must be built by <em>bitwise OR</em>'ing together
+     * (that is, using the <code>int</code> "|" operator) two or more
+     * of those <code>SWT</code> style constants. The class description
+     * lists the style constants that are applicable to the class.
+     * Style bits are also inherited from superclasses.
+     * </p>
+     *
+     * @param parent a composite control which will be the parent of the new instance (cannot be null)
+     * @param style the style of control to construct
+     * @param index the zero-relative index to store the receiver in its parent
+     *
+     * @exception IllegalArgumentException <ul>
+     *    <li>ERROR_NULL_ARGUMENT - if the parent is null</li>
+     *    <li>ERROR_INVALID_RANGE - if the index is not between 0 and the number of elements in the parent (inclusive)</li>
+     * </ul>
+     * @exception SWTException <ul>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the parent</li>
+     *    <li>ERROR_INVALID_SUBCLASS - if this class is not an allowed subclass</li>
+     * </ul>
+     *
+     * @see SWT
+     * @see Widget#checkSubclass
+     * @see Widget#getStyle
+     */
+    public FlutterTabItem(ITabFolder parent, int style, int index) {
+        super(parent, style);
+        this.parent = (FlutterTabFolder)parent;
+        createWidget(index);
+    }
+
+    /**
+     * Returns a rectangle describing the receiver's size and location
+     * relative to its parent.
+     *
+     * @return the receiver's bounding rectangle
+     *
+     * @exception SWTException <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     *
+     * @since 3.4
+     */
     public Rectangle getBounds() {
         return null;
     }
 
-    @Override
+    /**
+     * Returns the control that is used to fill the client area of
+     * the tab folder when the user selects the tab item.  If no
+     * control has been set, return <code>null</code>.
+     *
+     * @return the control
+     *
+     * @exception SWTException <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     */
     public IControl getControl() {
-        return null;
+        // Not Generated
+        return children != null && !children.isEmpty() ? (IControl) children.get(0) : null;
     }
 
-    @Override
+    /**
+     * Returns the receiver's parent, which must be a <code>TabFolder</code>.
+     *
+     * @return the receiver's parent
+     *
+     * @exception SWTException <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     */
     public ITabFolder getParent() {
-        return null;
+        checkWidget();
+        return parent;
     }
 
-    @Override
+    /**
+     * Returns the receiver's tool tip text, or null if it has
+     * not been set.
+     *
+     * @return the receiver's tool tip text
+     *
+     * @exception SWTException <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     */
     public String getToolTipText() {
-        return "";
+        return builder().getToolTipText().orElse(null);
     }
 
-    @Override
+    /**
+     * Sets the control that is used to fill the client area of
+     * the tab folder when the user selects the tab item.
+     *
+     * @param control the new control (or null)
+     *
+     * @exception IllegalArgumentException <ul>
+     *    <li>ERROR_INVALID_ARGUMENT - if the control has been disposed</li>
+     *    <li>ERROR_INVALID_PARENT - if the control is not in the same widget tree</li>
+     * </ul>
+     * @exception SWTException <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     */
     public void setControl(IControl control) {
-
-    }
-
-    @Override
-    public Image getImage() {
-        return null;
-    }
-
-    @Override
-    public String getText() {
-        return "";
+        // Not Generated
+        if (children == null) {
+            children = new ArrayList<>();
+        }
+        parent.children.remove(control);
+        children.add((FlutterWidget)control);
     }
 
     @Override
     public void setImage(Image image) {
-
     }
 
+    /**
+     * Sets the receiver's text.  The string may include
+     * the mnemonic character.
+     * <p>
+     * Mnemonics are indicated by an '&amp;' that causes the next
+     * character to be the mnemonic.  When the user presses a
+     * key sequence that matches the mnemonic, a selection
+     * event occurs. On most platforms, the mnemonic appears
+     * underlined but may be emphasised in a platform specific
+     * manner.  The mnemonic indicator character '&amp;' can be
+     * escaped by doubling it in the string, causing a single
+     * '&amp;' to be displayed.
+     * </p>
+     *
+     * @param string the new text
+     *
+     * @exception IllegalArgumentException <ul>
+     *    <li>ERROR_NULL_ARGUMENT - if the text is null</li>
+     * </ul>
+     * @exception SWTException <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     */
     @Override
     public void setText(String string) {
-
+        // Not Generated
+        super.setText(string);
     }
 
-    @Override
+    /**
+     * Sets the receiver's tool tip text to the argument, which
+     * may be null indicating that the default tool tip for the
+     * control will be shown. For a control that has a default
+     * tool tip, such as the Tree control on Windows, setting
+     * the tool tip text to an empty string replaces the default,
+     * causing no tool tip text to be shown.
+     * <p>
+     * The mnemonic indicator (character '&amp;') is not displayed in a tool tip.
+     * To display a single '&amp;' in the tool tip, the character '&amp;' can be
+     * escaped by doubling it in the string.
+     * </p>
+     * <p>
+     * NOTE: This operation is a hint and behavior is platform specific, on Windows
+     * for CJK-style mnemonics of the form " (&amp;C)" at the end of the tooltip text
+     * are not shown in tooltip.
+     * </p>
+     *
+     * @param string the new tool tip text (or null)
+     *
+     * @exception SWTException <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     */
     public void setToolTipText(String string) {
-
+        builder().setToolTipText(string);
+        FlutterSwt.dirty(this);
     }
 
-    @Override
-    public void addListener(int eventType, Listener listener) {
-
-    }
-
-    @Override
-    public void addDisposeListener(DisposeListener listener) {
-
-    }
-
-    @Override
-    public void dispose() {
-
-    }
-
-    @Override
-    public Object getData() {
-        return null;
-    }
-
-    @Override
-    public Object getData(String key) {
-        return null;
-    }
-
-    @Override
-    public IDisplay getDisplay() {
-        return null;
-    }
-
-    @Override
-    public Listener[] getListeners(int eventType) {
-        return new Listener[0];
-    }
-
-    @Override
-    public <L extends EventListener> Stream<L> getTypedListeners(int eventType, Class<L> listenerType) {
-        return Stream.empty();
-    }
-
-    @Override
-    public int getStyle() {
-        return 0;
-    }
-
-    @Override
-    public boolean isAutoDirection() {
-        return false;
-    }
-
-    @Override
-    public boolean isDisposed() {
-        return false;
-    }
-
-    @Override
-    public boolean isListening(int eventType) {
-        return false;
-    }
-
-    @Override
-    public void notifyListeners(int eventType, Event event) {
-
-    }
-
-    @Override
-    public void removeListener(int eventType, Listener listener) {
-
-    }
-
-    @Override
-    public void reskin(int flags) {
-
-    }
-
-    @Override
-    public void removeDisposeListener(DisposeListener listener) {
-
-    }
-
-    @Override
-    public void setData(Object data) {
-
-    }
-
-    @Override
-    public void setData(String key, Object value) {
-
-    }
-
-    @Override
-    public long getHandle() {
-        return 0;
+    public TabItemValue.Builder builder() {
+        if (builder == null)
+            builder = TabItemValue.builder().setId(handle).setStyle(style);
+        return (TabItemValue.Builder) builder;
     }
 }
