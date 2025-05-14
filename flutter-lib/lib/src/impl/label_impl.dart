a8861859355067a1bff55dcfa507a005ec58b9d2
EmilianaGirardi <girardiemiliana@gmail.com>
2025-05-14 13:32:14 -0300

    feat: add material Design implementation of Label

--- flutter-lib/lib/src/impl/label_impl.dart
+++ flutter-lib/lib/src/impl/label_impl.dart
@@ -1,39 +1,75 @@
-import 'package:fluent_ui/fluent_ui.dart';
-import 'control_impl.dart';
+import 'package:flutter/material.dart';
 import '../swt/swt.dart';
 import '../swt/label.dart';
-import '../styles.dart';
+import '../impl/control_impl.dart';
+import 'widget_config.dart';
+import 'package:swtflutter/src/styles.dart';
+import 'separatorlabel.dart';
 
 class LabelImpl<T extends LabelSwt<V>, V extends LabelValue>
     extends ControlImpl<T, V> {
+  final bool useDarkTheme = getCurrentTheme();
+
   @override
   Widget build(BuildContext context) {
-    var child = switch (StyleBits(state.style)) {
-      < SWT.SEPARATOR => Divider(
-          size: 10,
-          direction:
-              state.style.has(SWT.VERTICAL) ? Axis.vertical : Axis.horizontal,
-        ),
-      < SWT.VERTICAL => Column(children: [
-          RotatedBox(
-            quarterTurns: 1, // Rotate 90 degrees clockwise
-            child: buildText(),
-          )
-        ]),
-      _ => buildText(),
-    };
+    final text = state.text ?? '';
+    final enabled = state.enabled ?? true;
 
-    return super.wrap(child);
+    Widget child;
+
+    if (state.style.has(SWT.SEPARATOR)) {
+      child = SeparatorLabel(
+        direction: state.style.has(SWT.VERTICAL) ? Axis.vertical : Axis.horizontal,
+        useDarkTheme: useDarkTheme,
+      );
+    } else {
+      child = TextLabel(
+        text: text,
+        alignment: _getAlignment(),
+        wrap: state.style.has(SWT.WRAP),
+        useDarkTheme: useDarkTheme,
+        vertical: state.style.has(SWT.VERTICAL),
+      );
+    }
+
+    return MouseRegion(
+      onEnter: (_) => handleMouseEnter(),
+      onExit: (_) => handleMouseExit(),
+      child: Focus(
+        onFocusChange: (hasFocus) {
+          if (hasFocus) {
+            handleFocusIn();
+          } else {
+            handleFocusOut();
+          }
+        },
+        child: Opacity(
+          opacity: enabled ? 1.0 : 0.5,
+          child: child,
+        ),
+      ),
+    );
   }
 
-  Widget buildText() {
-    return Column(children: [
-      Text(
-        overflow: TextOverflow.ellipsis,
-        state.text ?? "",
-        softWrap: state.style.has(SWT.WRAP),
-        textAlign: TextAlign.start,
-      )
-    ]);
+  TextAlign _getAlignment() {
+    if (state.style.has(SWT.CENTER)) return TextAlign.center;
+    if (state.style.has(SWT.RIGHT)) return TextAlign.right;
+    return TextAlign.left;
+  }
+
+  void handleMouseEnter() {
+    widget.sendMouseTrackMouseEnter(state, null);
+  }
+
+  void handleMouseExit() {
+    widget.sendMouseTrackMouseExit(state, null);
+  }
+
+  void handleFocusIn() {
+    widget.sendFocusFocusIn(state, null);
+  }
+
+  void handleFocusOut() {
+    widget.sendFocusFocusOut(state, null);
   }
 }
