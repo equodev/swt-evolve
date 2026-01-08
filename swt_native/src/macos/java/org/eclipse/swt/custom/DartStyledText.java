a96e88861f570e060e133cb5764653c075aba5f4
Pia Bedini <pbedini@alumnos.exa.unicen.edu.ar>
2026-01-08 16:25:53 +0000

    fix: Stabilization of StyledText

--- swt_native/src/macos/java/org/eclipse/swt/custom/DartStyledText.java
+++ swt_native/src/macos/java/org/eclipse/swt/custom/DartStyledText.java
@@ -8651,6 +8651,7 @@ public class DartStyledText extends DartCanvas implements IStyledText {
      * @since 3.2
      */
     public void setAlignment(int alignment) {
+        dirty();
         checkWidget();
         if (!java.util.Objects.equals(this.alignment, alignment)) {
             dirty();
@@ -10617,10 +10618,8 @@ public class DartStyledText extends DartCanvas implements IStyledText {
      */
     public void setSelectionRange(int start, int length) {
         dirty();
-        Point newValue = new Point(selectionRange.x, selectionRange.y);
-        dirty();
-        this.selectionRange = newValue;
         setSelectionRanges(new int[] { start, length });
+        this.selectionRange = new Point(selection[0].x, selection[0].y - selection[0].x);
     }
 
     /**
@@ -10671,6 +10670,8 @@ public class DartStyledText extends DartCanvas implements IStyledText {
         }
         setSelection(fixedRanges, false, true);
         setCaretLocations();
+        this.selectionRange = new Point(selection[0].x, selection[0].y - selection[0].x);
+        this.selectionRanges = ranges;
     }
 
     /**
