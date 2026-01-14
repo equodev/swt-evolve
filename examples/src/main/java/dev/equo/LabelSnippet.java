9b0834f0fc972b79071aca4f173e9d06027fc56e
Emiliana Girardi <girardiemiliana@gmail.com>
2026-01-14 20:02:09 +0000

    feat: Stabilize Label for Windows

--- examples/src/main/java/dev/equo/LabelSnippet.java
+++ examples/src/main/java/dev/equo/LabelSnippet.java
@@ -1,6 +1,7 @@
 package dev.equo;
 
 import dev.equo.swt.Config;
+import dev.equo.swt.ConfigFlags;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
@@ -17,16 +18,17 @@ import org.eclipse.swt.widgets.Shell;
 public class LabelSnippet {
 
     public static void main (String[] args) {
-        Config.useEquo(Label.class);
+        ConfigFlags.use_swt_fonts(true);
         Display display = new Display();
-        Image image = new Image(display, LabelSnippet.class.getClassLoader().getResourceAsStream("swt-evolve.png"));
+        Image image = new Image(display, LabelSnippet.class.getClassLoader().getResourceAsStream("img.png"));
         Shell shell = new Shell (display);
         shell.setText("Label Snippet");
         shell.setSize(400, 200);
         Label label = new Label (shell, SWT.BORDER);
         label.setText ("Label with Image");
         label.setImage (image);
-        label.setSize(200, 100);
+        label.setSize(300, 150);
+        label.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_BLUE));
         shell.open ();
         while (!shell.isDisposed ()) {
             if (!display.readAndDispatch ()) display.sleep ();
