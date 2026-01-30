87df5ad99231e9cae67b22d7f5cf3c15716edb94
Lone Lion <1599112-lonelion@users.noreply.gitlab.com>
2026-01-30 02:24:25 +0000

    Fix ClassCastExceptions which happens on Fonts

--- swt_native/src/main/java/org/eclipse/swt/graphics/GraphicsUtils.java
+++ swt_native/src/main/java/org/eclipse/swt/graphics/GraphicsUtils.java
@@ -32,7 +32,7 @@ public class GraphicsUtils {
      * Note: getImageData() already returns a copy, so no additional cloning is needed here.
      *
      * @param display the display on which to create the new image
-     * @param image the image to copy (can be null)
+     * @param image   the image to copy (can be null)
      * @return a copy of the image as a DartImage, or null if input is null
      */
     public static Image copyImage(Display display, Image image) {
@@ -93,6 +93,47 @@ public class GraphicsUtils {
             return font;
         }
     }
+
+    /**
+     * Creates a SwtFont from any Font using only the basic font properties (name, height, style).
+     * If the font is already a SwtFont, it is returned as-is.
+     * If the font is a DartFont, a new SwtFont is created.
+     *
+     * @param font the font to convert (can be null)
+     * @return a SwtFont with the same name, height and style, or null if input is null
+     */
+    public static Font copyFontToSwt(Font font) {
+        if (font == null) {
+            return null;
+        }
+        if (font.getImpl() instanceof DartFont) {
+            FontData fd = font.getFontData()[0];
+            SwtFont swtFont = new SwtFont(font.getDevice(), fd.getName(), fd.getHeight(), fd.getStyle(), null);
+            return swtFont.getApi();
+        }
+        return font;
+    }
+
+    /**
+     * Creates a SwtFontData from any FontData using the basic font properties (name, height, style).
+     * If the fontData is already a SwtFontData, it is returned as-is.
+     * If the fontData is a DartFontData, a new SwtFontData is created.
+     *
+     * @param fontData the fontData to convert (can be null)
+     * @return a FontData with SwtFontData, or null if input is null
+     */
+    public static FontData copyFontDataToSwt(FontData fontData) {
+        if (fontData == null) {
+            return null;
+        }
+        if (fontData.getImpl() instanceof DartFontData) {
+            SwtFontData swtFontData = new SwtFontData(fontData.getName(), fontData.getHeight(), fontData.getStyle(), null);
+            return swtFontData.getApi();
+        }
+        return fontData;
+    }
+
+
     /**
      * Extracts the filename (without extension) from a given file path.
      *
