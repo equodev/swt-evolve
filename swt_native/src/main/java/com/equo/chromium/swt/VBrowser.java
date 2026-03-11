c66387420a0213a4bb767dd848047022a492235f
Guillermo Zunino <guillez@gmail.com>
2026-03-11 17:36:24 -0300

    feat: Dummy chromium

---
+++ swt_native/src/main/java/com/equo/chromium/swt/VBrowser.java
@@ -0,0 +1,66 @@
+package com.equo.chromium.swt;
+
+import com.dslplatform.json.*;
+import dev.equo.swt.Serializer;
+import org.eclipse.swt.widgets.VComposite;
+
+@CompiledJson()
+public class VBrowser extends VComposite {
+
+    protected VBrowser() {
+    }
+
+    protected VBrowser(DartBrowser impl) {
+        super(impl);
+    }
+
+    public boolean getJavascriptEnabled() {
+        return ((DartBrowser) impl).getJavascriptEnabled();
+    }
+
+    public void setJavascriptEnabled(boolean value) {
+        ((DartBrowser) impl).javascriptEnabled = value;
+    }
+
+    @JsonAttribute(nullable = false)
+    public String getText() {
+        return ((DartBrowser) impl).getText();
+    }
+
+    public void setText(String value) {
+        ((DartBrowser) impl).text = value;
+    }
+
+    public String getUrl() {
+        return ((DartBrowser) impl).getUrl();
+    }
+
+    public void setUrl(String value) {
+        ((DartBrowser) impl).url = value;
+    }
+
+    @JsonConverter(target = Browser.class)
+    public static class BrowserJson implements Configuration {
+
+        @Override
+        public void configure(DslJson json) {
+            json.registerWriter(DartBrowser.class, (JsonWriter.WriteObject<DartBrowser>) (writer, impl) -> {
+                Serializer.writeWithId(json, writer, impl);
+            });
+            json.registerReader(DartBrowser.class, (JsonReader.ReadObject<DartBrowser>) reader -> {
+                return null;
+            });
+        }
+
+        public static Browser read(JsonReader<?> reader) {
+            return null;
+        }
+
+        public static void write(JsonWriter writer, Browser api) {
+            if (api == null)
+                writer.writeNull();
+            else
+                writer.serializeObject(api.getImpl());
+        }
+    }
+}
