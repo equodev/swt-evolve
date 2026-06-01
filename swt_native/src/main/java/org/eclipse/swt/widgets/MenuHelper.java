d45f9da9933fd73a5639aa6f903567b612aa61c1
Agustina Pereyra <agusmapa@gmail.com>
2026-06-01 19:23:42 +0000

    fix: Tree menu closing on hover

--- swt_native/src/main/java/org/eclipse/swt/widgets/MenuHelper.java
+++ swt_native/src/main/java/org/eclipse/swt/widgets/MenuHelper.java
@@ -5,10 +5,6 @@ import dev.equo.swt.FlutterBridge;
 
 public class MenuHelper {
 
-    static DartMenu activePopupMenu = null;
-    static Listener focusOutListener = null;
-    static Listener mouseDownFilter = null;
-
     static void hookEvents(DartMenu menu) {
         FlutterBridge.on(menu, "Help", "Help", e -> {
             menu.getDisplay().asyncExec(() -> {
@@ -17,21 +13,11 @@ public class MenuHelper {
         });
         FlutterBridge.on(menu, "Menu", "Hide", e -> {
             menu.getDisplay().asyncExec(() -> {
-                if (MenuHelper.activePopupMenu == menu) {
-                    MenuHelper.activePopupMenu = null;
-                }
-                removeOwnerFocusListener(menu);
                 menu.sendEvent(SWT.Hide, e);
             });
         });
         FlutterBridge.on(menu, "Menu", "Show", e -> {
             menu.getDisplay().asyncExec(() -> {
-                DartMenu previous = MenuHelper.activePopupMenu;
-                if (previous != null && previous != menu) {
-                    FlutterBridge.sendEvent(previous, "closeMenu");
-                }
-                MenuHelper.activePopupMenu = menu;
-                addOwnerFocusListener(menu);
                 try {
                     menu.sendEvent(SWT.Show, e);
                 } catch (RuntimeException ex) {
@@ -39,28 +25,6 @@ public class MenuHelper {
                 menu.dirty();
             });
         });
-    }
-
-    static void addOwnerFocusListener(DartMenu menu) {
-        if (MenuHelper.focusOutListener != null) return;
-        Control owner = menu.findOwnerControl();
-        if (owner == null || owner.isDisposed()) return;
-        MenuHelper.focusOutListener = event -> closeIfActive(menu, event.widget, owner);
-        menu.getDisplay().addFilter(SWT.FocusIn, MenuHelper.focusOutListener);
-        MenuHelper.mouseDownFilter = event -> {
-            if (event.button == 1) closeIfActive(menu, event.widget, owner);
-        };
-        menu.getDisplay().addFilter(SWT.MouseDown, MenuHelper.mouseDownFilter);
-    }
-
-    static void closeIfActive(DartMenu menu, Widget clickedWidget, Control owner) {
-        if (!menu.isDisposed() && MenuHelper.activePopupMenu == menu && clickedWidget != owner) {
-            menu.getDisplay().asyncExec(() -> {
-                if (!menu.isDisposed() && MenuHelper.activePopupMenu == menu) {
-                    FlutterBridge.sendEvent(menu, "closeMenu");
-                }
-            });
-        }
     }
 
     static MenuItem[] removeItem(MenuItem[] items, MenuItem item) {
@@ -77,13 +41,4 @@ public class MenuHelper {
         return newItems;
     }
 
-    static void removeOwnerFocusListener(DartMenu menu) {
-        if (MenuHelper.focusOutListener == null) return;
-        menu.getDisplay().removeFilter(SWT.FocusIn, MenuHelper.focusOutListener);
-        MenuHelper.focusOutListener = null;
-        if (MenuHelper.mouseDownFilter != null) {
-            menu.getDisplay().removeFilter(SWT.MouseDown, MenuHelper.mouseDownFilter);
-            MenuHelper.mouseDownFilter = null;
-        }
-    }
 }
