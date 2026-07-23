b61341c400dcb3c48302593620ee466fd3bfd25e
Agustin Schilling <schillingagustin@gmail.com>
2026-07-23 03:18:27 +0000

    fix(gc): don't reject a null image before the zero-size early return

--- swt_native/src/embedWindows/java/org/eclipse/swt/graphics/DartGC.java
+++ swt_native/src/embedWindows/java/org/eclipse/swt/graphics/DartGC.java
@@ -911,8 +911,6 @@ public final class DartGC extends DartResource implements IGC {
      * </ul>
      */
     public void drawImage(Image image, int srcX, int srcY, int srcWidth, int srcHeight, int destX, int destY, int destWidth, int destHeight) {
-        if (image == null)
-            SWT.error(SWT.ERROR_NULL_ARGUMENT);
         checkNonDisposed();
         if (srcWidth == 0 || srcHeight == 0 || destWidth == 0 || destHeight == 0)
             return;
